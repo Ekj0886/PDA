1cc283db292ce1514d8acb228d5fd21e0b0a1edc
ekj <kuochiayung@gmail.com>
2024-11-28 14:04:27 +0800

    initial commit

---
+++ Lab4/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+//#include"HEADER_FILE_NAME.h"
+
+#define F first 
+#define S second
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    string input_gmp  = argv[1];
+    string input_gcl  = argv[2];
+    string input_cst  = argv[3];
+    string output_lg  = argv[4];
+
+    
+    
+    
+    return 0;
+
+}
---
+++ Lab4/run.sh
@@ -0,0 +1,4 @@
+make
+./D2DGRter testcase/testcase0/testcase0.gmp testcase/testcase0/testcase0.gcl testcase/testcase0/testcase0.cst output/testcase0.lg
+./D2DGRter testcase/testcase1/testcase1.gmp testcase/testcase1/testcase1.gcl testcase/testcase1/testcase1.cst output/testcase1.lg
+./D2DGRter testcase/testcase2/testcase2.gmp testcase/testcase2/testcase2.gcl testcase/testcase2/testcase2.cst output/testcase2.lg
